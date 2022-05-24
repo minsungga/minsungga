5a269c0613143f2d08ddb1bec6f2c452fbfb9f7c
minsungga <106140601+minsungga@users.noreply.github.com>
2022-05-24 16:57:20 +0900

    Create MQ_message.java

---
+++ MQ_message.java
@@ -0,0 +1,14 @@
+package test;
+
+public class MyMessage {
+	String id = null;
+	String msg = null;
+	boolean isUsed = false;
+	
+	public MyMessage(String id, String msg) {
+		this.id = id;
+		this.msg = msg;
+		this.isUsed = false;
+	}	
+
+}
